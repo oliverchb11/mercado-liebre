4c3c41480a99436f1b0db26bb123201986f6564b
Oliver Charry <oliver.charry.216@unisabaneta.edu.co>
2022-07-18 16:57:53 -0500

    tercer avance pagina mercado liebre v0.4

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
 const routerHome = require('./routers/home.routers')
 const routerRegister = require('./routers/register.router')
 const routerLogin = require('./routers/login.router')
 
-
-
-//static files
+app.use(cors());
+app.set('puerto', process.env.PORT || 3000)
+    //static files
 console.log(__dirname);
 app.use('/', express.static(__dirname + '/public'))
     //routers
@@ -16,6 +16,6 @@ app.use('/', routerRegister)
 app.use('/', routerLogin)
 
 
-app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
+app.listen(app.get('puerto'), () => {
+    console.log(`Servidor corriendo en el puerto ${app.get('puerto')}`);
 })
