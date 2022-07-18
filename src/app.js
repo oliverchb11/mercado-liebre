547ffcf1f7b9a8ecd9df99050e9b9a2924b297f8
Oliver Charry <oliver.charry.216@unisabaneta.edu.co>
2022-07-18 18:56:35 -0500

    prueba

--- src/app.js
+++ src/app.js
@@ -4,18 +4,17 @@ const app = express();
 const routerHome = require('./routers/home.routers')
 const routerRegister = require('./routers/register.router')
 const routerLogin = require('./routers/login.router')
-
+    //middlewares
 app.use(cors());
 app.set('puerto', process.env.PORT || 3000)
     //static files
-console.log(__dirname);
 app.use('/', express.static(__dirname + '/public'))
     //routers
 app.use('/', routerHome)
 app.use('/', routerRegister)
 app.use('/', routerLogin)
 
-
+// listen server
 app.listen(app.get('puerto'), () => {
     console.log(`Servidor corriendo en el puerto ${app.get('puerto')}`);
 })
